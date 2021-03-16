47047b8d319951710f0d8486c8fa940a62bd2ff9
bezdomnij <attlila.novak@yahoo.co.uk>
2021-03-17 00:06:10 +0100

    started with templates, directories

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Home"
+    return render_template("home.html")
 
 # 127.0.0.1
